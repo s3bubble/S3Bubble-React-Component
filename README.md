fe3d076e03665e9443afd28120d6c57f1ba49298
S3bubble <support@s3bubble.com>
2022-05-21 14:46:05 +0100

    drm fixes

---
+++ README.md
@@ -0,0 +1,53 @@
+# S3Bubble DRM Video Player
+
+![S3Bubble Video Player](https://s3b-assets-bucket.s3.amazonaws.com/screenshot_2022_02_16_at_18.43.50.png)
+
+# S3Bubble DRM Audio Waveform Player
+
+![S3Bubble Audio Player](https://s3b-assets-bucket.s3.amazonaws.com/audio-player.png)
+
+![View Docs Here For More Information](https://www.npmjs.com/package/@s3bubble/player)
+
+```
+npm install @s3bubble/s3player-react-component
+```
+## DRM Example
+```js
+import './App.css';
+import S3Player from "@s3bubble/s3player-react-component";
+
+function App() {
+    return (<>
+        <S3Player setup={
+            {
+                type: '<video | audio>',
+                drm: {
+                    authenticationXml: '<authenticationXml>',
+                    widevine: {
+                        code: '<widevine code>',
+                        keysystem: 'com.widevine.alpha',
+                        license: 'https://wv-keyos.licensekeyserver.com/'
+                    },
+                    playready: {
+                        code: '<playready code>',
+                        keysystem: 'com.microsoft.playready',
+                        license: 'https://pr-keyos.licensekeyserver.com/core/rightsmanager.asmx'
+                    },
+                    fairplay: {
+                        code: '<fairplay code>',
+                        keysystem: 'com.apple.fps.1_0',
+                        license: 'https://fp-keyos.licensekeyserver.com/getkey',
+                        certificate: 'https://00000000000.cloudfront.net/fairplay.cer'
+                    },
+                }
+            }
+        } />
+    </>
+    );
+}
+
+export default App;
+```
+
+## Video Tutorial
+[open demo](https://www.youtube.com/watch?v=9F_SbFPCz_0)
--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 const S3Player = (props) => {
-    // You can edit this component to suit your needs
-    const link = document.createElement("link");
-    link.href = "https://unpkg.com/@s3bubble/player@latest/dist/css/s3bubble.min.css";
-    link.type = "text/css";
-    link.rel = "stylesheet";
-    link.media = "screen,print";
-    document.body.appendChild(link);
 
-    const script = document.createElement("script");
-    script.src = "https://unpkg.com/@s3bubble/player@latest/dist/js/s3bubble.min.js";
-    script.async = true;
-    document.body.appendChild(script);
+    const uniqueId = Date.now();
 
-    const attributes = {
-        className: 's3bmedia',
-    }
-    for (const [key, value] of Object.entries(props.setup)) {
-        attributes[`data-${key}`] = value;
-    }
+    useEffect(() => {
 
-    return (<div {...attributes}></div>);
+        const link = document.createElement("link");
+        link.href = "https://unpkg.com/@s3bubble/player@latest/dist/css/s3bubble.min.css";
+        link.type = "text/css";
+        link.rel = "stylesheet";
+        link.media = "screen,print";
+        document.body.appendChild(link);
+
+        const script = document.createElement("script");
+        script.src = "https://unpkg.com/@s3bubble/player@latest/dist/js/s3bubble.min.js";
+        script.async = true;
+        document.body.appendChild(script);
+
+        if (props.setup.drm) {
+            // eslint-disable-next-line no-undef
+            s3bubble(`s3media-${uniqueId}`).drm(props.setup);
+        } else {
+            // eslint-disable-next-line no-undef
+            s3bubble(`s3media-${uniqueId}`).media(props.setup);
+        }
+
+    }, [props.setup, uniqueId]);
+
+    return (<div id={`s3media-${uniqueId}`} className='s3media'></div>);
 }
 export default S3Player;
